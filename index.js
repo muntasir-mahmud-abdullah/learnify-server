a3dab04b1d45e03d6bee5eb9ea3753ff62153ea8
muntasir-mahmud-abdullah <muntasir525@gmail.com>
2024-12-25 14:59:35 +0600

    my posted tutorial section updated

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.werzz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.werzz.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -20,91 +20,130 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-    // languages related apis
-    const languagesCollection = client.db("learnify").collection("languages");
+    console.log("Successfully connected to MongoDB!");
+
+    // Collections
+    const db = client.db("learnify");
+    const languagesCollection = db.collection("languages");
+    const tutorsCollection = db.collection("tutors");
+    const tutorialsCollection = db.collection("tutorials");
+
+    // Routes
+    // Languages APIs
     app.get("/languages", async (req, res) => {
-      const cusor = languagesCollection.find();
-      const result = await cusor.toArray();
-      res.send(result);
+      try {
+        const result = await languagesCollection.find().toArray();
+        res.send(result);
+      } catch (error) {
+        res.status(500).send({ error: "Failed to fetch languages" });
+      }
     });
+
     app.get("/languages/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await languagesCollection.findOne(query);
-      res.send(result);
+      try {
+        const id = req.params.id;
+        if (!ObjectId.isValid(id)) {
+          return res.status(400).send({ error: "Invalid ID format" });
+        }
+        const result = await languagesCollection.findOne({ _id: new ObjectId(id) });
+        res.send(result);
+      } catch (error) {
+        res.status(500).send({ error: "Failed to fetch language" });
+      }
     });
-    // tutors related apis
-    const tutorsCollection = client.db("learnify").collection("tutors");
-    const bookedTutorsCollection = client
-      .db("learnify")
-      .collection("booked_tutors");
-    const tutorialsCollection = client.db("learnify").collection("tutorials");
+
+    // Tutors APIs
     app.get("/tutors", async (req, res) => {
-      const cusor = tutorsCollection.find();
-      const result = await cusor.toArray();
-      res.send(result);
+      try {
+        const result = await tutorsCollection.find().toArray();
+        res.send(result);
+      } catch (error) {
+        res.status(500).send({ error: "Failed to fetch tutors" });
+      }
     });
+
     app.get("/tutors/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await tutorsCollection.findOne(query);
-      res.send(result);
+      try {
+        const id = req.params.id;
+        if (!ObjectId.isValid(id)) {
+          return res.status(400).send({ error: "Invalid ID format" });
+        }
+        const result = await tutorsCollection.findOne({ _id: new ObjectId(id) });
+        res.send(result);
+      } catch (error) {
+        res.status(500).send({ error: "Failed to fetch tutor" });
+      }
     });
 
-    // post tutorial
-
+    // Tutorials APIs
     app.post("/tutorials", async (req, res) => {
-      const tutorial = req.body;
-      const result = await tutorialsCollection.insertOne(tutorial);
-      res.send(result);
+      try {
+        const tutorial = req.body;
+        const result = await tutorialsCollection.insertOne(tutorial);
+        res.send(result);
+      } catch (error) {
+        res.status(500).send({ error: "Failed to post tutorial" });
+      }
     });
 
-    //my tutorials
     app.get("/tutorials", async (req, res) => {
-      const email = req.query.email;
-      let query = null;
-      if (email) {
-        query = { email: email };
+      try {
+        const email = req.query.email;
+        const query = email ? { email } : {};
+        const result = await tutorialsCollection.find(query).toArray();
+        res.send(result);
+      } catch (error) {
+        res.status(500).send({ error: "Failed to fetch tutorials" });
       }
-      const cusor = tutorialsCollection.find(query);
-      const result = await cusor.toArray();
-      res.send(result);
     });
 
-    //Booked Tutors apis
-    //get all data ,get one data , get some dat [0,1,many]
-    app.get("/booked-tutor", async (req, res) => {
-      const email = req.query.email;
-      const query = { user_email: email };
-      const result = await bookedTutorsCollection.find(query).toArray();
-      //aggregation of data
-      // name,image, language,price,and review btn
-      for (const bookedTutor of result) {
-        // console.log(bookedTutor.tutor_id);
-      }
-      res.send(result);
-    });
 
-    app.post("/booked-tutors", async (req, res) => {
-      const booked = req.body;
-      const result = bookedTutorsCollection.insertOne(booked);
-      res.send(result);
-    });
-
-    // fetch booked tutor by id
-
-    app.get("/myTutors/:id", async (req, res) => {
+    // Update tutorial
+    app.put('/tutorials/:id',async(req,res)=>{
       const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await tutorsCollection.findOne(query);
-      res.send(result);
+      const filter = {_id:new ObjectId(id)};
+      const options = {upsert:true};
+      const updatedTutorial = req.body;
+      const tutorial = {
+        $set: {
+          name: updatedTutorial.name,
+          image: updatedTutorial.image,
+          language: updatedTutorial.language,
+          description: updatedTutorial.description,
+          price: updatedTutorial.price,
+        },
+      }
+      const result = await tutorialsCollection.updateOne(filter,tutorial,options)
+      res.send(result)
+    })
+
+
+//delete tutorial
+    app.delete("/tutorials/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        if (!ObjectId.isValid(id)) {
+          return res.status(400).send({ error: "Invalid ID format" });
+        }
+        const result = await tutorialsCollection.deleteOne({ _id: new ObjectId(id) });
+        res.send(result);
+      } catch (error) {
+        res.status(500).send({ error: "Failed to delete tutorial" });
+      }
+    });
+
+    app.get("/tutorials/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        if (!ObjectId.isValid(id)) {
+          return res.status(400).send({ error: "Invalid ID format" });
+        }
+        const result = await tutorialsCollection.findOne({ _id: new ObjectId(id) });
+        res.send(result);
+      } catch (error) {
+        res.status(500).send({ error: "Failed to fetch tutorial" });
+      }
     });
   } finally {
     // Ensures that the client will close when you finish/error
@@ -114,8 +153,9 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("respond send");
+  res.send("Learnify server is running");
 });
+
 app.listen(port, () => {
-  console.log("port listening at:", port);
+  console.log("Server is listening at port:", port);
 });
